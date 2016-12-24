a4655495e5de89b6e84c9b7bfdd82f6e965b0188
LittleStupid <lttlstpd@live.com>
2016-12-24 16:15:47 +0800

    update

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -2,17 +2,30 @@ from django.conf.urls import url
 from rango import views
 from django.conf import settings
 from django.conf.urls.static import static
+# from django.contrib.staticfiles import views
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index')
 ]
 
-if settings.DEBUG:
-    urlpatterns += [
-        url('django.views.static', views.index),
-        url(r'^media/(?P<path>.*)', views.index),
-        url('serve', views.index),
-        url({'document_root': settings.MEDIA_ROOT}, views.index)]
+# if settings.DEBUG:
+#     urlpatterns += [
+#         url(r'^media/(?P<path>.*)$', views.serve, {
+#             'document_root': settings.MEDIA_ROOT,
+#         }),
+#     ]
+#
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
+#
 
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += [
+#         url('django.views.static', views.index),
+#         url(r'^media/(?P<path>.*)', views.index),
+#         url('serve', views.index),
+#         url({'document_root': settings.MEDIA_ROOT}, views.index)]
+#
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
