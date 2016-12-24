1ddf695e8a0fafb94bc45fa16a43fb93d9aac640
LittleStupid <lttlstpd@live.com>
2016-12-24 15:56:43 +0800

    media directory

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,6 +1,18 @@
 from django.conf.urls import url
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index')
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url('django.views.static', views.index),
+        url(r'^media/(?P<path>.*)', views.index),
+        url('serve', views.index),
+        url({'document_root': settings.MEDIA_ROOT}, views.index)]
+
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
