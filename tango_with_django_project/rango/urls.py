4b11610d43753f6abbaeeb31b366dc7e38a1aee4
LittleStupid <lttlstpd@live.com>
2016-12-24 21:18:32 +0800

    user login/logout

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     url(r'^add_category/$', views.add_category,
         name='add_category'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^restricted/', views.restricted, name='restricted'),
+    url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.category, name='category')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
