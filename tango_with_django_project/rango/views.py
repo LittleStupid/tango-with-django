960daf62dbf60814e48b3b0289c35f2291534cfa
LittleStupid <lttlstpd@live.com>
2016-12-24 14:50:07 +0800

    add template/change directory

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Rango says hello here!")
+    context_dict = {'boldmessage': 'I am bold from the message!'}
+    return render(request, 'rango/index.html', context_dict)
